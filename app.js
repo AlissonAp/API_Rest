f36e0ca2f3951f885975bca7ddfd5d2c788a1aa2
AlissonAp <alisson.allebrandtt@gmail.com>
2017-09-19 20:16:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+console.log('Antes de chamar configs');
+
 const app = require('./config/app_config');
 const db  = require('./config/db_config');
+
+console.log('antes de chamar os models');
 
 const pecas           = require('./models/pecas');
 const pecaController  = require('./controllers/pecaController');
 const RotaPecas       = require('./routes/pecasRouter');
 const RotaMissoes     = require('./routes/missoesRouter');
+
+console.log('Antes de chamar o roteamento');
 
 app.get('/',function(req,res){
   res.end('Bem vindo a API de manutenção de peças e missões');
