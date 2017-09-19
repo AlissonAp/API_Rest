831d45f32f4bb43b6d0aa4e975460ff28821f027
AlissonAp <alisson.allebrandtt@gmail.com>
2017-09-19 19:29:22 -0300

    Update db_config.js

--- config/db_config.js
+++ config/db_config.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
-const urlString = 'mongodb://localhost/API_NotebookLine';
+const urlString = 'mongodb://admin:root@ds139844.mlab.com:39844/thenotebookline';
 
 mongoose.connect(urlString, function(err, res){
   if(err){
     console.log('Não foi possível conectar com a base de dados '+urlString);
+      console.log(err);
   }else{
     console.log('Conexão com o banco de dados realizada com sucesso!');
+
   }
 });
