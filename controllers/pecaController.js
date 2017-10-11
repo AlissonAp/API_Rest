84aada5df47518312dcae127c715b9cfe7be844a
Diego Schmidt <Diego Schmidt>
2017-10-11 01:32:16 -0300

    Alteração sugerida pecasController

--- controllers/pecaController.js
+++ controllers/pecaController.js
@@ -3,87 +3,110 @@ let Peca = require('../models/pecas');
 let Propriedades = require('../models/propriedadesPeca');
 let ObjectId = require('mongoose').Types.ObjectId;
 
-exports.testepropriedade = function(propriedades,callback){
+// Outra forma de realizar isso
+// module.exports = {
 
- console.log(propriedades);
+//   salvaPropriedade(propriedade, resultado) {
 
-  let propriedade = new Propriedades(JSON.parse(propriedades)).save(function(error, propriedade){
-    if(error){
-      callback({error: 'Não foi possível salvar a propriedade'+error});
-    }else{
+//     Propriedades.save(JSON.parse(propriedade), function (erro, retorno) {
+
+//       if (erro) {
+//         console.log("Erro: " + erro);
+//       } else {
+//         console.log("Erro: " + erro);
+//       }
+
+//       resultado(erro, retorno);
+
+//     });
+
+//   }
+
+
+// }
+
+
+exports.testepropriedade = function (propriedades, callback) {
+
+  console.log(propriedades);
+
+  let propriedade = new Propriedades(JSON.parse(propriedades)).save(function (error, propriedade) {
+    if (error) {
+      callback({ error: 'Não foi possível salvar a propriedade' + error });
+    } else {
       console.log(propriedade);
       callback(propriedade);
     }
   });
 }
 
-exports.save = function(pardescricao,parcategoria,parinformacoes,parpreco,parpropriedades,parnivel,parimagem,callback){
+exports.save = function (pardescricao, parcategoria, parinformacoes, parpreco, parpropriedades, parnivel, parimagem, callback) {
 
   //Salva as propriedades da peça
   let propriedade = new Propriedades(parpropriedades)
 
 
 
-  let peca  = new Peca({
-    descricao      : pardescricao,
-    categoria      : parcategoria,
-    informacoes    : parinformacoes,
-    preco          : parpreco,
-    propriedades   : propriedade, //Neste aqui salva o ID do JSON de propriedade
-    nivel          : parnivel,
-    imagem         : parimagem,
+  let peca = new Peca({
+    descricao: pardescricao,
+    categoria: parcategoria,
+    informacoes: parinformacoes,
+    preco: parpreco,
+    propriedades: propriedade, //Neste aqui salva o ID do JSON de propriedade
+    nivel: parnivel,
+    imagem: parimagem,
     JsonPropriedades: propriedade
   });
 
-/*
-  Peca.findOne(peca)
-      .populate('propriedades')
-      .exec(function(error, propriedade) {
-          console.log(JSON.stringify(propriedade, null, "\t"))
-      })
-  */
-  peca.save(function(error, peca){
-      if(!error){
-        callback(peca);
-        console.log('SAlvou');
-      }else{
-      callback({error: 'Não foi possível cadastrar a peça'});
-      console.log('Deu pau',error)
+  /*
+    Peca.findOne(peca)
+        .populate('propriedades')
+        .exec(function(error, propriedade) {
+            console.log(JSON.stringify(propriedade, null, "\t"))
+        })
+    */
+  peca.save(function (error, peca) {
+    if (!error) {
+      callback(peca);
+      console.log('SAlvou');
+    } else {
+      callback({ error: 'Não foi possível cadastrar a peça' });
+      console.log('Deu pau', error)
     }
   });
 }
 
-exports.list = function(callback){
+exports.list = function (callback) {
 
-  Peca.find({}, function(error, peca){
-    if(error){
-      callback({error: 'Não foi possível encontrar as peças'});
-    }else{
+  Peca.find({}, function (error, peca) {
+    if (error) {
+      callback({ error: 'Não foi possível encontrar as peças' });
+    } else {
       callback(peca);
     }
 
   });
 }
 
-exports.listCategoria = function(categoria,callback){
-  Peca.find({categoria}, function(error, peca){
-    if(error){
-      callback({error: 'Não foi possível encontrar as peças'});
-    }else{
+exports.listCategoria = function (categoria, callback) {
+  Peca.find({ categoria }, function (error, peca) {
+    if (error) {
+      callback({ error: 'Não foi possível encontrar as peças' });
+    } else {
       callback(peca);
     }
 
   });
 }
 
-exports.delete = function(id,callback){
-  Peca.findById(id, function(error,peca){
-    if(error){
-      callback({error:'Não foi possível excluir'});
-    }else{
-      peca.remove(function(error){
-        if(!error){
-          callback({resposta:"Peça excluída com sucesso!"});
+exports.delete = function (id, callback) {
+  Peca.findById(id, function (error, peca) {
+    if (error) {
+      callback({ error: 'Não foi possível excluir' });
+    } else {
+      peca.remove(function (error) {
+        if (!error) {
+          callback({ resposta: "Peça excluída com sucesso!" });
         }
       })
     }
