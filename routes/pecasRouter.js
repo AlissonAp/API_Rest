9096135d5ceb46097ddd319a740f9f0124f44206
Diego Schmidt <diegohschmidt@gmail.com>
2017-10-30 21:45:26 -0200

    Alteração peças

--- routes/pecasRouter.js
+++ routes/pecasRouter.js
@@ -36,42 +36,7 @@ router.post('/cadastrar', function (req, res) {
 
   console.log(req.body);
 
-  let peca = new Peca({
-    descricao: req.body.descricao,
-    categoria: req.body.categoria,
-    informacoes: req.body.informacoes,
-    preco: req.body.preco,
-    propriedades: {
-      GbMemoriaRam: req.body.propriedades.GbMemoriaRam,
-      GbPlacaVideo: req.body.propriedades.GbPlacaVideo,
-      GbArmazenamento: req.body.propriedades.GbArmazenamento,
-      MhzMemoriaRam: req.body.propriedades.MhzMemoriaRam,
-      GhzProcessador: req.body.propriedades.GhzProcessador,
-      GhzPlacaVideo: req.body.propriedades.GhzPlacaVideo,
-      RpmLeituraEscrita: req.body.propriedades.RpmLeituraEscrita,
-      NucleosProcessador: req.body.propriedades.NucleosProcessador,
-      ModeloProcessador: req.body.propriedades.ModeloProcessador,
-      BitsPlacaVideo: req.body.propriedades.BitsPlacaVideo,
-      cacheProcessador: req.body.propriedades.cacheProcessador,
-      cacheArmazenamento: req.body.propriedades.cacheArmazenamento,
-      MahBateria: req.body.propriedades.MahBateria,
-      CelulasBateria: req.body.propriedades.CelulasBateria,
-      TipoTela: req.body.propriedades.TipoTela,
-      TamanhoTela: req.body.propriedades.TamanhoTela,
-      ConexoesUSB: req.body.propriedades.ConexoesUSB,
-      PossuiBluetooth: req.body.propriedades.PossuiBluetooth,
-      PossuiWebCam: req.body.propriedades.PossuiWebCam,
-      PossuiLeitorCd_Dvd: req.body.propriedades.PossuiLeitorCd_Dvd,
-      ResistenciaCarcaca: req.body.propriedades.ResistenciaCarcaca,
-      PesoCarcaca: req.body.propriedades.PesoCarcaca,
-      PossuiEntradaHDMI: req.body.propriedades.PossuiEntradaHDMI,
-      SistemaOperacional: req.body.propriedades.SistemaOperacional 
-    },     
-    nivel: req.body.nivel,
-    imagem: req.body.imagem,
-  });
-
-  pecasController.save(peca).then((peca) => {
+  pecasController.save(req).then((peca) => {
     res.json(peca);
   }).catch((error) => {
     console.log(error);
