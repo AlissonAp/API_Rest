02c045c273384f276667d73c747c54e1aca07d87
AlissonAp <alisson.allebrandtt@gmail.com>
2017-11-02 16:23:47 -0200

    Ajuste conflito usuario

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -33,7 +33,6 @@ module.exports = {
 
         let msg = ""
 
-<<<<<<< HEAD
         if (usuario.length) {
           msg = "Usuario atualizado com sucesso!"
 
@@ -47,38 +46,19 @@ module.exports = {
 
           });
 
-=======
-        console.log(user);
-
-        if (user.length) {
-          usuario.id = user.id
-          Usuario.update(usuario).then((usuario) => {
-            msg = "Usuario atualizado com sucesso!"
-            resolve(retorno(202,true,"Usuário atualizado com sucesso!"));
-          }).catch((error) => {
-            reject(retorno(500,false,error));
-          });
->>>>>>> c8a5b6055ae887ad1a4dbe4767219feed5adf2c9
         } else {
           
           msg = "Usuário criado com sucesso!"
-<<<<<<< HEAD
 
-=======
->>>>>>> c8a5b6055ae887ad1a4dbe4767219feed5adf2c9
           new Usuario(usuario).save().then((usuario) => {
             resolve(retorno(200, true, msg));
           }).catch((error) => {
             reject(500, false, "Houve uma falha ao criar o novo usuário!");
           });
-<<<<<<< HEAD
 
         }
 
-
-=======
         }
->>>>>>> c8a5b6055ae887ad1a4dbe4767219feed5adf2c9
       }).catch((error) => {
         reject(retorno(500, false, error));
       });
