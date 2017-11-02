c448331a2a368bd6867e2109804208c62e151385
AlissonAp <alisson.allebrandtt@gmail.com>
2017-11-02 16:19:47 -0200

    Ajuste conflito

--- models/missoes.js
+++ models/missoes.js
@@ -10,20 +10,13 @@ const dataCadastro  = require('../fields/DataAtual');
 const XP            = require("../fields/TipoNumber");
 
 let MissoesSchema = new Schema({
-
+  
   nome,
   objetivo,
-<<<<<<< HEAD
-  regras : { type: Schema.Types.ObjectId, ref: 'Regras'},
-  dataCadastro, //Seta a data atual por padrão
-  JsonRegras,
-  xp,
-  dinheiro
-=======
   regras : regras,
   dataCadastro, //Seta a data atual por padrão  
   XP,
->>>>>>> 1e81c1d0259bf830cbfd4dc1a6b24a04c3986b31
+
 });
 
 module.exports = db.model('Missoes', MissoesSchema);
